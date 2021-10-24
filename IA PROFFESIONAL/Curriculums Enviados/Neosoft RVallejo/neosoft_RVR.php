1f392b8f9c3602229fb357aa9e093c9bb330d03a
ricardo.vallejo76@gmail.com <ricardo.vallejo76@gmail.com>
2021-10-24 17:33:29 -0400

    mis a jour IA Non supervise et IA Profesional

---
+++ IA PROFFESIONAL/Curriculums Enviados/Neosoft RVallejo/neosoft_RVR.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>neosoft</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="stylemisrestos.css" rel="stylesheet" type="text/css" />
+   
+      <script type='text/javascript' src="js/tablefilter_all_min.js"></script>
+    
+  
+    
+      <link rel="stylesheet" type="text/css" href="filtergrid.css" />
+    
+  
+    
+      <script type='text/javascript' src="js/ezEditTable.js"></script>
+      <style type="text/css">
+<!--
+.Estilo1 {
+	color: #0033FF;
+	font-weight: bold;
+	font-size: 24px;
+	font-style: italic;
+}
+.Estilo2 {color: #FF0000}
+body {
+	margin-left: 100px;
+	margin-top: 50px;
+	margin-right: 100px;
+	margin-bottom: 50px;
+}
+-->
+      </style>
+</head>
+<body>
+<div id="topPanel"></div>
+<div>
+  <div align="center">
+    <p>&nbsp;</p>
+    <p align="justify">Bonjour M. Carpentier, M. St-Laurent,</p>
+    <p align="justify">Selon notre derni&egrave;re conversation, l'envoi de certaines applications  LabVIEW. Les applications avec  mat&eacute;riel ouvrira &eacute;chou&eacute; parce que, je n ai pas la disponibilit&eacute; des drivers n&eacute;cessaires.</p>
+    <p align="justify">Les programmes  que je donne sont ma propri&eacute;t&eacute;, je ne manque de respect envers la  confidentialit&eacute; d&rsquo;aucune entreprise.</p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify"><span class="Estilo1">1) Wattview</span> is a triphasic  measure  tool.&nbsp;</p>
+    <p align="justify">Cet outil a &eacute;t&eacute; d&eacute;velopp&eacute; en utilisant Labview 5, 6,  6i. et 7.</p>
+    <ul>
+      <li>
+        <div align="justify">Datasocket Server. Web reports. Manipulation et l'enregistrement des quantit&eacute;s massives de donn&eacute;es. NI DAQ Cards (PCI). </div>
+      </li>
+      <li>
+        <div align="justify">Advanced Signal Processing.  </div>
+      </li>
+    </ul>
+    <p align="justify">Selon les ing&eacute;nieurs sur le terrain &eacute;taient les besoins, de nouvelles caract&eacute;ristiques ont &eacute;t&eacute; d&eacute;velopp&eacute;es. &Eacute;galement un assistant pour cr&eacute;er des graphiques pour l'analyse des donn&eacute;es enregistr&eacute;es a &eacute;t&eacute; d&eacute;velopp&eacute;.</p>
+    <p align="justify"><a href="Wattview Postes Electriques/Wattview virtual Aplicacion.llb">Download Wattview Software </a>(LLB)</p>
+    <p align="justify"><a href="Wattview Postes Electriques/Wattview Reportes.llb">Download Wattview reports</a> (LLB)</p>
+    <p align="justify">Ci-joint, un document descriptif en anglais de l'outil, qui a &eacute;t&eacute; expos&eacute;e au NI Week 2004 &agrave; la Colombie.</p>
+    <p align="justify"><a href="Wattview Postes Electriques/Product Wattview english.docx">Download Wattview document</a>  DOC </p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify"><span class="Estilo1">2) VMAP </span> Outil pour l'acquisition de signaux, pour l'analyse de la combustion dans les moteurs d'automobile.</p>
+    <p align="justify">Labview 6i.</p>
+    <ul>
+      <li>
+        <div align="justify">Mesure de la vitesse en utilisant une carte ATMIO.<br />
+          Obtenir des signaux analogiques avec la tecnique -clock counter- et codeur.<br />
+          Triggers Analogique et num&eacute;rique pour la d&eacute;tection des temps de combustion. (<strong>moteurs  </strong><strong>&agrave; 2 temps et 4 temps) </strong> <br />
+          Commande d'&eacute;lectrovanne et banc hydraulique.<br />
+          Mesure simultan&eacute;e de la vitesse, des angles de rotation et des signaux analogiques diff&eacute;rents. 
+          <p>&nbsp;</p>
+          </div>
+      </li>
+    </ul>
+    <p align="justify"><a href="VMAP Combustion/VMAP_llb.llb">Download Software VMAP</a> </p>
+    <p align="justify"><a href="Articulo.Scientia.pdf">Article Review Scientia et Tecnica pour cette logicielle</a></p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify"><span class="Estilo1">3</span><span class="Estilo1">) VOLTA - Reactive </span>Outil de calcul de condensateurs pour la compensation de puissance r&eacute;active, en utilisant des m&eacute;thodologies diff&eacute;rentes.</p>
+    <ul>
+      <li>
+        <div align="justify">Labview 8.5</div>
+      </li>
+      <li>
+        <div align="justify">Project, executable, installer et documentation, Development file structure. </div>
+      </li>
+    </ul>
+    <p align="justify">VI principale du projet est: <span class="Estilo2">Compensacion2.vi</span></p>
+    <p align="justify"><a href="VOLTA - Reactive.zip">Download VOLTA - Reactive </a></p>
+    <p align="justify"><span class="Estilo1">4) VOLTA - Electrique </span> Outil pour le calcul des alimentateurs secondaires dans les syst&egrave;mes de distribution d'&eacute;nergie sous diff&eacute;rentes r&egrave;gles et normes.</p>
+    <ul>
+      <li>
+        <div align="left">Labview 8.5</div>
+      </li>
+      <li>
+        <div align="left">Project, executable, installer et documentation, Development file structure. </div>
+      </li>
+    </ul>
+    <p align="justify"> VI principale du projet est: <span class="Estilo2">MOD_Conductores.vi</span></p>
+    <p align="justify"><a href="VOLTA - Electrique.zip">Download software Volta Electrique</a> </p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify">&nbsp;</p>
+    <p align="justify">&nbsp;</p>
+    <h2>&nbsp;</h2>
+  </div>
+</div>
+ <div id="footerPanel"><div id="footerbodyPanel"></div>
+</div>
+</body>
+</html>
